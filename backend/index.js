b4dfc16e5855c784e4320c1b6697a6289d288709
Sai Narsimha Reddy Keesari <k.sainarsimhareddy03@gmail.com>
2024-03-30 23:41:14 -0500

    added

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const io = socketIo(server, {
     credentials: true
   }
 });
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 connectToMongo();
 app.use(cors());
 app.use(express.json());

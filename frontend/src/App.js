3e9eeeff4a64321165b96b1476560f739aa04b1f
Sai Narsimha Reddy Keesari <k.sainarsimhareddy03@gmail.com>
2024-03-31 00:22:45 -0500

    vercel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,7 @@ import { useEffect } from 'react';
 import io from 'socket.io-client';
 
 function App() {
-  const socket = io('http://localhost:5000');
+  const socket = io('http://realtime-polling.vercel.app');
   useEffect(() => {
     return () => {
       socket.disconnect();
